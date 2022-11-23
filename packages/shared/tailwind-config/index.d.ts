3836015ba65506784a2eb4b7951ddce45d7090c6
fellipeutaka <fellipeutaka@gmail.com>
2022-11-23 10:55:06 -0300

    feat: TailwindCSS types

--- packages/shared/tailwind-config/index.d.ts
+++ packages/shared/tailwind-config/index.d.ts
@@ -1,7 +1,7 @@
 import type { DefaultColors } from "tailwindcss/types/generated/colors";
 import type { Config } from "tailwindcss/types/config";
 
-declare module "@nlw-copa/tailwind-config" {
+declare module "@encontrei/tailwind-config" {
   export const config: Config;
   export const colors: DefaultColors;
 }
